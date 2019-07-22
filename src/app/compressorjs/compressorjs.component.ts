3542cb959702b9e6f3c447ff5fb6d3ecfee2b499
zombaki <piyush.nath.2272@gmail.com>
2019-07-22 14:55:47 -0500

    1 file modified, 1 added

--- src/app/compressorjs/compressorjs.component.ts
+++ src/app/compressorjs/compressorjs.component.ts
@@ -1,47 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import Compressor from 'compressorjs';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-compressorjs',
   templateUrl: './compressorjs.component.html',
   styleUrls: ['./compressorjs.component.css']
 })
 export class CompressorjsComponent implements OnInit {
-file: any;
-file_data: File;
+  file: any;
+  file_data: File;
+  mySrc: any;
+  final:File;
   constructor() { }
 
   ngOnInit() {
   }
-  upload(){
-    console.log("this is a test",this.file_data);
+  upload() {
+    const _result = this;
+    console.log("this is a test", this.file_data);
     new Compressor(this.file_data, {
-    quality: 0.6,
-    success(result) {
-      const formData = new FormData();
-      console.log(result);
-      // The third parameter is required for server
-      formData.append('file', result, 'Data');
-      this.file_data = result;
-      // Send the compressed image file to server with XMLHttpRequest.
-      // axios.post('/path/to/upload', formData).then(() => {
-      //   console.log('Upload success');
-      // });
-    },
-    error(err) {
-      console.log(err.message);
-    },
-  });
+      quality: 0.6,
+      success(result) {
+        console.log("this is a result", result); 
+        this.final = result;
+        _result.getBase64FromFile(_result.file_data).then(p => _result.mySrc = p);
+      },
+      error(err) {
+        console.log(err.message);
+      },
+    });
   }
   onFileSelected(event) {
- if(event.target.files.length > 0) 
-  {
-    this.file_data = event.target.files[0];
-    console.log(event.target.files[0].name);
+    if (event.target.files.length > 0) {
+      this.file_data = event.target.files[0];
+    }
   }
-}
   public getBase64FromFile(file: File): Promise<string> {
-    console.log('this is a file',file);
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
 
@@ -59,29 +54,3 @@ file_data: File;
   }
 }
 
- 
-// document.getElementById('file').addEventListener('change', (e) => {
-//   const file = e.target.files[0];
- 
-//   if (!file) {
-//     return;
-//   }
- 
-//   new Compressor(file, {
-//     quality: 0.6,
-//     success(result) {
-//       const formData = new FormData();
- 
-//       // The third parameter is required for server
-//       formData.append('file', result, result.name);
- 
-//       // Send the compressed image file to server with XMLHttpRequest.
-//       axios.post('/path/to/upload', formData).then(() => {
-//         console.log('Upload success');
-//       });
-//     },
-//     error(err) {
-//       console.log(err.message);
-//     },
-//   });
-// });
